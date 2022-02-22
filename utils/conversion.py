25a2d41a1e85134a045ad72d9883e286831720c4
Pasquale Roseti <229627@studenti.unimore.it>
2022-02-22 17:26:56 +0100

    modified conversion file

--- utils/conversion.py
+++ utils/conversion.py
@@ -2,9 +2,9 @@
 import pandas as pd
 
 
-def conversion(dataset, nodes=None):
+def conversion_2(dataset, nodes=None):
     # Avoid warnings
-    pd.options.mode.chained_assignment = None
+    # pd.options.mode.chained_assignment = None
 
     df = dataset.copy()
 
@@ -47,3 +47,64 @@ def conversion(dataset, nodes=None):
     df = df.astype(int)
 
     return df
+
+
+def conversion(dataset, nodes=None):
+
+    df = dataset.copy()
+
+    # Converting the values from Double to Int (Binary)
+    # T
+    if 'T' in df.columns:
+        df.loc[df['T'] <= 298.15, 'T'] = 0
+        df.loc[df['T'] > 298.15, 'T'] = 1
+
+    # O
+    if 'O' in df.columns:
+        df.loc[df['O'] <= 290.15, 'O'] = 0
+        df.loc[df['O'] > 290.15, 'O'] = 1
+
+    # H
+    if 'H' in df.columns:
+        df.loc[df['H'] <= 500, 'H'] = 0
+        df.loc[df['H'] > 500, 'H'] = 1
+
+    # C
+    if 'C' in df.columns:
+        df.loc[df['C'] <= 500, 'C'] = 0
+        df.loc[df['C'] > 500, 'C'] = 1
+
+    # Pow
+    if 'Pow' in df.columns:
+        df.loc[df['Pow'] <= 100000, 'Pow'] = 0
+        df.loc[df['Pow'] > 100000, 'Pow'] = 1
+
+    # CO
+    if 'CO' in df.columns:
+        df.loc[df['CO'] <= 15, 'CO'] = 0
+        df.loc[df['CO'] > 15, 'CO'] = 1
+
+    # CO2
+    if 'CO2' in df.columns:
+        df.loc[df['CO2'] <= 1400, 'CO2'] = 0
+        df.loc[df['CO2'] > 1400, 'CO2'] = 1
+
+    df = df.astype(int)
+
+    return df
+
+
+# if __name__ == '__main__':
+#     dataset = pd.read_csv("dataset.csv", sep=',').drop(columns=['timestamp'])[:5]
+#     print(dataset)
+#
+#     # mod1 = conversion(dataset)
+#     # print(mod1)
+#
+#     # mod2 = conversion_2(dataset)
+#     # print(mod2)
+
+
+
+
+
