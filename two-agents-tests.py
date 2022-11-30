547089e4d8641c8006e5281f8a53ec21ce8f2d0a
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2022-11-30 17:41:24 +0100

    refactorings

--- two-agents-tests.py
+++ two-agents-tests.py
@@ -1,4 +1,4 @@
-# This file contains a communication example between two agents
+# DOC This file contains a communication example between two agents
 # In particular, agent 1 and agent 2, where agent 2 sends a request to agent 1
 # The request contains nodes that the agent 2 finds as incomplete or nodes from undirected edges
 # If there are not such nodes, no message will be sent
@@ -15,7 +15,10 @@ import time
 # TODO put in config file
 from utils.logging import append_to_report
 
-REPORT = 'multi_agent_' + str(datetime.datetime.now()) + '.txt'
+now = datetime.datetime.now()
+timestamp = datetime.datetime.timestamp(now)
+REPORT = 'two_agents_' + str(timestamp)
+OUTPUT_DIR = "output/reproducibility/"
 
 online_parameters = {
     'max_cond_vars': 4,
@@ -24,14 +27,14 @@ online_parameters = {
     'ci_conf': 0.1
 }
 
-agent_1_parameters = {
+agent_1red_params = {
     'max_cond_vars': 4,
     'do_size': 500,
     'do_conf': 0.9,
     'ci_conf': 0.4
 }
 
-agent_2_parameters = {
+agent_2blue_params = {
     'max_cond_vars': 4,
     'do_size': 500,
     'do_conf': 0.9,
@@ -104,59 +107,61 @@ def write_results(gt, pred, elapsed_time, description):
     append_to_report(f"\n{description}", file=REPORT)
 
     gt_nodes = nodes_from_edges(gt)
-    gt_edges = pred
+    gt_edges = gt  # NB was 'pred'
     pred_nodes = nodes_from_edges(pred)
     pred_edges = pred
 
     # Print figures
-    dot, new_edges, missed_edges, recovered_edges = difference(gt, pred, stat=True)
+    dot, fp, fn, tp = difference(gt, pred, stat=True)
     # dot.view(directory=f'tmp/test/{output_name}')
 
     gt_net = f"\nGround-truth \nNodes: {gt_nodes}\tlen={len(gt_nodes)} \nEdges: {gt_edges}\tlen={len(gt_edges)}"
     pred_net = f"\nPredicted \nNodes: {pred_nodes}\tlen={len(pred_nodes)} \nEdges: {pred_edges}\tlen={len(pred_edges)}"
-    missed = f"\nMissed edges: {missed_edges}"
+    missed = f"\nMissed edges: {fn}"
     comp_time = f"\nComputational time: {elapsed_time} s"
     results = gt_net + pred_net + missed + comp_time
     append_to_report(results, file=REPORT)
 
     # Edge statistics
     n_edges = len(gt)  # Ground-truth number of edges
-    new_edges = len(new_edges)
-    missed_edges = len(missed_edges)
-    recovered_edges = len(recovered_edges)
+    fp = len(fp)
+    fn = len(fn)
+    tp = len(tp)
 
     # Performance measure
-    edge_results = f'\nNew edges: {new_edges} \nMissed edges: {missed_edges} \nRecovered edges: {recovered_edges}'
+    edge_results = f'\nFalse positives: {fp} \nFalse negatives: {fn} \nTrue positives: {tp}'
     append_to_report(edge_results, file=REPORT)
-    recover_rate = f'\nRecover rate: {(recovered_edges / n_edges) * 100} %'
+    recover_rate = f'\nRecover rate: {(tp / n_edges) * 100} %'
     append_to_report(recover_rate, file=REPORT)
-    missed_rate = f'\nMissed rate: {(missed_edges / n_edges) * 100} %'
+    missed_rate = f'\nMissed rate: {(fn / n_edges) * 100} %'
     append_to_report(missed_rate, file=REPORT)
+    # shd = f'\nSHD mod: {fp + fn + unknown} %'  # TODO
+    # append_to_report(shd, file=REPORT)
 
 
-# Double-agent Learning
-def run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate, timestamp):
+# 2-agent protocol run
+def run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate):
     start = time.time()
-    # 1 - Offline Local learning (Agent 1 and Agent 2)
+    # 1 - Local learning (Agent 1 and Agent 2)
     model_1, undirected_edges_1 = agent_1.learning(nodes=agent_1.nodes, non_doable=agent_1.non_doable,
-                                                   parameters=agent_1_parameters, mod='offline', bn=agent_1.gt_bn,
+                                                   parameters=agent_1red_params, mod='offline', bn=agent_1.gt_bn,
                                                    obs_data=agent_1.obs_data)
-    time_1 = (time.time() - start)
+    time_1 = (time.time() - start)  # local learning time consumption
     agent_1.replace_edges(list(model_1.edges()))
     agent_1.add_undirected_edges(undirected_edges_1)
-    dot = difference(gt_1, model_1.edges())  # compare gt and pred
-    dot.view(filename='1', directory=f'tmp/{timestamp}/')
+    dot = difference(gt_1, model_1.edges())  # compare gt and learnt
+    dot.view(filename=f"{REPORT}_1", directory=OUTPUT_DIR)
     write_results(gt_1, model_1.edges(), time_1, "\nAgent 1 network")
 
     start = time.time()
     model_2, undirected_edges_2 = agent_2.learning(nodes=agent_2.nodes, non_doable=agent_2.non_doable,
-                                                   parameters=agent_2_parameters, mod='offline', bn=agent_2.gt_bn,
+                                                   parameters=agent_2blue_params, mod='offline', bn=agent_2.gt_bn,
                                                    obs_data=agent_2.obs_data)
-    time_2 = (time.time() - start)
+    time_2 = (time.time() - start)  # local learning time consumption
     agent_2.replace_edges(list(model_2.edges()))
     agent_2.add_undirected_edges(undirected_edges_2)
-    dot = difference(gt_2, model_2.edges())  # compare gt and pred
-    dot.view(filename='2', directory=f'tmp/{timestamp}/')
+    dot = difference(gt_2, model_2.edges())  # compare gt and learnt
+    dot.view(filename=f"{REPORT}_2", directory=OUTPUT_DIR)
     write_results(gt_2, model_2.edges(), time_2, "\nAgent 2 network")
 
     # 2 - Request (Agent 2)
@@ -164,8 +169,8 @@ def run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate, timestamp):
         # Nodes in the message are the ones indicated from the user (still manually) and the ones
         # from undirected edges discovered by the local learning algorithm
         msg = agent_2.build_request_msg(nodes_to_investigate, agent_2.undirected_edges)
-        append_to_report(f'\n\nUndirected edges: {agent_2.undirected_edges}', file=REPORT)
-        append_to_report(f'\nNodes to investigate: {nodes_to_investigate}', file=REPORT)
+        append_to_report(f'\n\nUndirected edges: {agent_2.undirected_edges}', file=f"{REPORT}.txt")
+        append_to_report(f'\nNodes to investigate: {nodes_to_investigate}', file=f"{REPORT}.txt")
         print('Request message ', msg)
     else:
         print('No communication, ending.')
@@ -194,7 +199,7 @@ def run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate, timestamp):
         time_3 = (time.time() - start)
         # The response contains the learnt edges
         response = agent_1.build_response_msg(model.edges())
-        append_to_report(f'\nOnline predicted edges: {model.edges()}', file=REPORT)
+        append_to_report(f'\nOnline predicted edges: {model.edges()}', file=f"{REPORT}.txt")
     else:  # If we receive False, we check edges already known by agent 1
         response = get_response_edges(agent_1.edges, nodes_to_investigate)
     print('Response: ', response)
@@ -212,55 +217,53 @@ def run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate, timestamp):
     # difference function needs ground-truth network to visualize results
     # dot = difference(agent_2.edges, model_2.edges())
     dot = difference(gt_3, agent_2.edges)
-    dot.view(filename='3', directory=f'tmp/{timestamp}/')
+    dot.view(filename=f"{REPORT}_3", directory=OUTPUT_DIR)
     write_results(gt_3, agent_2.edges, time_3, "\nAgent 2 network after request")
 
     return model_1, model_2, response['edges'], elapsed_time
 
 
 if __name__ == '__main__':
-    now = datetime.datetime.now()
-    timestamp = datetime.datetime.timestamp(now)
-    append_to_report(f'\n\n#### {now}', file=REPORT)
-    append_to_report(f'\ntimestamp: {timestamp}', file=REPORT)
-    append_to_report(f'\nMulti-agent learning', file=REPORT)
+    append_to_report(f'\n\n#### {now}', file=f"{REPORT}.txt")
+    append_to_report(f'\ntimestamp: {timestamp}', file=f"{REPORT}.txt")
+    append_to_report(f'\nMulti-agent learning', file=f"{REPORT}.txt")
 
     notes = ""
-    append_to_report(f'\nNotes:\t{notes}', file=REPORT)
+    append_to_report(f'\nNotes:\t{notes}', file=f"{REPORT}.txt")
 
-    append_to_report('\n\nOffline agent 1 parameters:', file=REPORT)
-    for par in agent_1_parameters:
-        append_to_report(f'\n{par} = {agent_1_parameters[par]}', file=REPORT)
+    append_to_report('\n\nOffline agent 1 parameters:', file=f"{REPORT}.txt")
+    for par in agent_1red_params:
+        append_to_report(f'\n{par} = {agent_1red_params[par]}', file=f"{REPORT}.txt")
 
-    append_to_report('\n\nOffline agent 2 parameters:', file=REPORT)
-    for par in agent_2_parameters:
-        append_to_report(f'\n{par} = {agent_2_parameters[par]}', file=REPORT)
+    append_to_report('\n\nOffline agent 2 parameters:', file=f"{REPORT}.txt")
+    for par in agent_2blue_params:
+        append_to_report(f'\n{par} = {agent_2blue_params[par]}', file=f"{REPORT}.txt")
 
-    append_to_report('\n\nOnline parameters:', file=REPORT)
+    append_to_report('\n\nOnline parameters:', file=f"{REPORT}.txt")
     for par in online_parameters:
-        append_to_report(f'\n{par} = {online_parameters[par]}', file=REPORT)
+        append_to_report(f'\n{par} = {online_parameters[par]}', file=f"{REPORT}.txt")
 
     # Choose how to divide the network
-    network_1 = networks.create_gt_net_skel(['Pr', 'L', 'Pow', 'S', 'H', 'C'], False)
-    network_2 = networks.create_gt_net_skel(['W', 'O', 'T', 'B', 'A', 'CO', 'CO2'], False)
+    net_1red = networks.create_gt_net_skel(['Pr', 'L', 'Pow', 'S', 'H', 'C'], False)
+    net_2blue = networks.create_gt_net_skel(['W', 'O', 'T', 'B', 'A', 'CO', 'CO2'], False)
 
     # Initialize agents
-    agent_1 = Agent(nodes=network_1['nodes'], non_doable=network_1['non_doable'], edges=network_1['edges'],
-                    obs_data=network_1['dataset'])
-    agent_2 = Agent(nodes=network_2['nodes'], non_doable=network_2['non_doable'], edges=network_2['edges'],
-                    obs_data=network_2['dataset'])
+    agent_1red = Agent(nodes=net_1red['nodes'], non_doable=net_1red['non_doable'], edges=net_1red['edges'],
+                       obs_data=net_1red['dataset'])
+    agent_2blue = Agent(nodes=net_2blue['nodes'], non_doable=net_2blue['non_doable'], edges=net_2blue['edges'],
+                        obs_data=net_2blue['dataset'])
 
     # List the nodes to send in the message from agent 2 to agent 1
     # So far, this array is managed manually, but the idea is to build an automatic method to recognize the nodes to
     # investigate from outliers values
-    nodes_to_investigate = ['T']
+    frontier = ['T']
 
     # Ground-truth agent 1
-    gt_1 = [("Pr", "L"), ("Pr", "S"), ("L", "Pow"), ("S", "H"), ("H", "Pow"), ("S", "C"), ("C", "Pow")]
+    gt_1red_pre = [("Pr", "L"), ("Pr", "S"), ("L", "Pow"), ("S", "H"), ("H", "Pow"), ("S", "C"), ("C", "Pow")]
     # Ground-truth agent 2
-    gt_2 = [("CO", "A"), ("CO2", "A"), ("A", "W"), ("B", "W"), ("O", "T"), ("W", "T")]
+    gt_2blue = [("CO", "A"), ("CO2", "A"), ("A", "W"), ("B", "W"), ("O", "T"), ("W", "T")]
     # Ground-truth agent 2 post request
-    gt_3 = [("H", "T"), ("C", "T"), ("CO", "A"), ("CO2", "A"), ("A", "W"), ("B", "W"), ("O", "T"), ("W", "T")]
+    gt_2blue_post = [("H", "T"), ("C", "T"), ("CO", "A"), ("CO2", "A"), ("A", "W"), ("B", "W"), ("O", "T"), ("W", "T")]
 
     # Run the entire algorithm
-    model_1, model_2, new_edges, elapsed_time = run(agent_1, agent_2, gt_1, gt_2, gt_3, nodes_to_investigate, timestamp)
+    model_1red, model_2blue, new_edges, elapsed_time = run(agent_1red, agent_2blue, gt_1red_pre, gt_2blue, gt_2blue_post, frontier)
